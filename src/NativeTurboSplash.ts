6a22f86061d915f54bc6a8956ade620c7f54c649
mhp23 <hpousty@gmail.com>
2025-02-05 20:52:30 +0330

    Revert "chore: updating the Spec type"
    This reverts commit 6478eb97328a6e6c7823f167c6fc6052e4d74a50.

--- src/NativeTurboSplash.ts
+++ src/NativeTurboSplash.ts
@@ -1,9 +1,6 @@
 import type {TurboModule} from 'react-native';
 import {TurboModuleRegistry} from 'react-native';
 
-export interface Spec extends TurboModule {
-  show: () => void;
-  hide: () => void;
-}
+export interface Spec extends TurboModule {}
 
 export default TurboModuleRegistry.getEnforcing<Spec>('TurboSplash');
